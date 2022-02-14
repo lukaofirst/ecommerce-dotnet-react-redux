f72586c453a9e286a39fb7b5dd4676cac784034f
lukaofirst <lucasmacedo123@gmail.com>
2022-02-14 17:17:40 -0300

    adding and fetching basket using context

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Catalog from '../../features/catalog/Catalog';
 import Header from './Header';
 import {
@@ -17,9 +17,29 @@ import 'react-toastify/dist/ReactToastify.css';
 import ServerError from '../errors/ServerError';
 import NotFound from '../errors/NotFound';
 import BasketPage from '../../features/basket/BasketPage';
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
-    const [darkMode, setDarkMode] = useState<boolean>(false);
+    const { setBasket } = useStoreContext();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const buyerId = getCookie('buyerId');
+        if (buyerId) {
+            agent.Basket.get()
+                .then((basket) => setBasket(basket))
+                .catch((error) => console.log(error))
+                .finally(() => setLoading(false));
+        } else {
+            setLoading(false);
+        }
+    }, [setBasket]);
+
+    const [darkMode, setDarkMode] = useState(false);
+
     const paletteType = darkMode ? 'dark' : 'light';
 
     const theme = createTheme({
@@ -31,9 +51,11 @@ function App() {
         },
     });
 
-    const handleThemeChange = () => {
-        setDarkMode((prevState) => !prevState);
-    };
+    function handleThemeChange() {
+        setDarkMode(!darkMode);
+    }
+
+    if (loading) return <LoadingComponent message='Initialising app...' />;
 
     return (
         <ThemeProvider theme={theme}>
@@ -46,8 +68,8 @@ function App() {
             <Header handleThemeChange={handleThemeChange} />
             <Container>
                 <Switch>
-                    <Route path='/' exact component={HomePage} />
-                    <Route path='/catalog' exact component={Catalog} />
+                    <Route exact path='/' component={HomePage} />
+                    <Route exact path='/catalog' component={Catalog} />
                     <Route path='/catalog/:id' component={ProductDetails} />
                     <Route path='/about' component={AboutPage} />
                     <Route path='/contact' component={ContactPage} />
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import App from './app/layout/App';
+import { StoreProvider } from './app/context/StoreContext';
 
 export const customHistory = createBrowserHistory();
 
 ReactDOM.render(
     <React.StrictMode>
         <Router history={customHistory}>
-            <App />
+            <StoreProvider>
+                <App />
+            </StoreProvider>
         </Router>
     </React.StrictMode>,
     document.getElementById('root')
