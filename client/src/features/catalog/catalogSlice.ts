d7dac6e2b6b68ae3ee09083d59223eee2d02a59c
lukaofirst <lucasmacedo123@gmail.com>
2022-02-17 10:48:38 -0300

    error handling in the catalog and basket reducers

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -11,22 +11,22 @@ const productsAdapter = createEntityAdapter<Product>();
 
 export const fetchProductsAsync = createAsyncThunk<Product[]>(
     'catalog/fetchProductsAsync',
-    async () => {
+    async (_, thunkAPI) => {
         try {
             return await agent.Catalog.list();
-        } catch (error) {
-            console.log(error);
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
         }
     }
 );
 
 export const fetchProductAsync = createAsyncThunk<Product, number>(
     'catalog/fetchProductAsync',
-    async (productId) => {
+    async (productId, thunkAPI) => {
         try {
             return await agent.Catalog.details(productId);
-        } catch (error) {
-            console.log(error);
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
         }
     }
 );
@@ -49,7 +49,8 @@ export const catalogSlice = createSlice({
             state.productsLoaded = true;
         });
 
-        builder.addCase(fetchProductsAsync.rejected, (state) => {
+        builder.addCase(fetchProductsAsync.rejected, (state, action) => {
+            console.log(action.payload);
             state.status = 'idle';
         });
 
@@ -62,7 +63,8 @@ export const catalogSlice = createSlice({
             state.status = 'idle';
         });
 
-        builder.addCase(fetchProductAsync.rejected, (state) => {
+        builder.addCase(fetchProductAsync.rejected, (state, action) => {
+            console.log(action.payload);
             state.status = 'idle';
         });
     },
