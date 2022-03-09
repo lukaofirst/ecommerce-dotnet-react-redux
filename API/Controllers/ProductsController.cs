24dc6cbff6508d591ef75291955e25b14f2c2e8d
lukaofirst <lucasmacedo123@gmail.com>
2022-03-09 10:12:52 -0300

    adding automapper and CRUD operations to products

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,58 +5,112 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTO;
 using API.Entities;
 using API.Extensions;
 using API.RequestHelpers;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
-    public class ProductsController : BaseApiController
-    {
-        private readonly StoreContext _context;
+	public class ProductsController : BaseApiController
+	{
+		private readonly StoreContext _context;
+		private readonly IMapper _mapper;
 
-        public ProductsController(StoreContext context)
-        {
-            _context = context;
-        }
+		public ProductsController(StoreContext context, IMapper mapper)
+		{
+			_mapper = mapper;
+			_context = context;
+		}
 
-        [HttpGet]
-        public async Task<ActionResult<PagedList<Product>>> Get([FromQuery] ProductParams productParams)
-        {
-            var query = _context.Products
-                .Sort(productParams.OrderBy)
-                .Search(productParams.SearchTerm)
-                .Filter(productParams.Brands, productParams.Types)
-                .AsQueryable();
+		[HttpGet]
+		public async Task<ActionResult<PagedList<Product>>> Get([FromQuery] ProductParams productParams)
+		{
+			var query = _context.Products
+				.Sort(productParams.OrderBy)
+				.Search(productParams.SearchTerm)
+				.Filter(productParams.Brands, productParams.Types)
+				.AsQueryable();
 
-            var products = await PagedList<Product>.ToPagedList(query,
-                productParams.PageNumber, productParams.PageSize);
+			var products = await PagedList<Product>.ToPagedList(query,
+				productParams.PageNumber, productParams.PageSize);
 
-            Response.AddPaginationHeader(products.MetaData);
+			Response.AddPaginationHeader(products.MetaData);
 
-            return products;
-        }
+			return products;
+		}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetById(int id)
-        {
-            var product = await _context.Products.FindAsync(id);
+		[HttpGet("{id}", Name = "GetProduct")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			var product = await _context.Products.FindAsync(id);
 
-            if (product == null) return NotFound();
+			if (product == null) return NotFound();
 
-            return Ok(product);
-        }
+			return Ok(product);
+		}
 
-        [HttpGet("filters")]
-        public async Task<IActionResult> GetFilters()
-        {
-            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
-            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+		[HttpGet("filters")]
+		public async Task<IActionResult> GetFilters()
+		{
+			var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+			var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
 
-            return Ok(new { brands, types });
-        }
-    }
+			return Ok(new { brands, types });
+		}
+
+		[Authorize(Roles = "Admin")]
+		[HttpPost]
+		public async Task<ActionResult<Product>> CreateProduct(CreateProductDTO productDTO)
+		{
+			var product = _mapper.Map<Product>(productDTO);
+
+			_context.Products.Add(product);
+
+			var result = await _context.SaveChangesAsync() > 0;
+
+			if (result) return CreatedAtRoute("GetProduct", new { Id = product.Id }, product);
+
+			return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
+		}
+
+		[Authorize(Roles = "Admin")]
+		[HttpPut]
+		public async Task<ActionResult> UpdateProduct(UpdateProductDTO productDTO)
+		{
+			var product = await _context.Products.FindAsync(productDTO.Id);
+
+			if (product == null) return NotFound();
+
+			_mapper.Map(productDTO, product);
+
+			var result = await _context.SaveChangesAsync() > 0;
+
+			if (result) return NoContent();
+
+			return BadRequest(new ProblemDetails { Title = "Problem updating product" });
+		}
+
+		[Authorize(Roles = "Admin")]
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> DeleteProduct(int id)
+		{
+			var product = await _context.Products.FindAsync(id);
+
+			if (product == null) return NotFound();
+
+			_context.Products.Remove(product);
+
+			var result = await _context.SaveChangesAsync() > 0;
+
+			if (result) return Ok();
+
+			return BadRequest(new ProblemDetails { Title = "Problem deleting product" });
+		}
+	}
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,122 +2,127 @@ using System.Text;
 using API.Data;
 using API.Entities;
 using API.Middleware;
+using API.RequestHelpers;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "Jwt auth header",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
+			services.AddControllers();
 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
-                    }
-                });
-            });
+			services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
-            services.AddDbContext<StoreContext>(opt =>
-            {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Description = "Jwt auth header",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer"
+				});
 
-            services.AddCors();
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							},
+							Scheme = "oauth2",
+							Name = "Bearer",
+							In = ParameterLocation.Header
+						},
+						new List<string>()
+					}
+				});
+			});
 
-            services.AddIdentityCore<User>(opt =>
-            {
-                opt.User.RequireUniqueEmail = true;
-            })
-                .AddRoles<Role>()
-                .AddEntityFrameworkStores<StoreContext>();
+			services.AddDbContext<StoreContext>(opt =>
+			{
+				opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+			});
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(opt =>
-                {
-                    opt.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTSettings:TokenKey"]))
-                    };
-                });
+			services.AddCors();
 
-            services.AddAuthorization();
-            services.AddScoped<TokenService>();
-        }
+			services.AddIdentityCore<User>(opt =>
+			{
+				opt.User.RequireUniqueEmail = true;
+			})
+				.AddRoles<Role>()
+				.AddEntityFrameworkStores<StoreContext>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<ExceptionMiddleware>();
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(opt =>
+				{
+					opt.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuer = false,
+						ValidateAudience = false,
+						ValidateLifetime = true,
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTSettings:TokenKey"]))
+					};
+				});
 
-            if (env.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+			services.AddAuthorization();
+			services.AddScoped<TokenService>();
+		}
 
-            // app.UseHttpsRedirection();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			app.UseMiddleware<ExceptionMiddleware>();
 
-            app.UseRouting();
+			if (env.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+			}
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+			// app.UseHttpsRedirection();
 
-            app.UseCors(opt =>
-            {
-                opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
-            });
+			app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapFallbackToController("Index", "Fallback");
-            });
-        }
-    }
+			app.UseCors(opt =>
+			{
+				opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+			});
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+				endpoints.MapFallbackToController("Index", "Fallback");
+			});
+		}
+	}
 }
